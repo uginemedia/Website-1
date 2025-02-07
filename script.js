4c596dda1ffb2b64f871894ee9ba1a8ac87770c8
U-GINE MEDIA <113266902+uginemedia@users.noreply.github.com>
2025-02-07 19:33:24 +0000

    Deleted script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("scroll", function () {
-  let birds = document.querySelectorAll(".birds");
-  let scrollPosition = window.scrollY;
-  birds.forEach((bird) => {
-    bird.style.transform = `translateX(${scrollPosition * 0.5}px)`;
-  });
-});
